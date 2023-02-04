0cec4b44b9466a46cfb6eab1a99c380650a695dc
Jean Augusto Rego <96090625+JeanRego@users.noreply.github.com>
2023-02-04 01:27:02 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,7 +0,0 @@
-
-from twilio.rest import Client
-
-accont_sid = "ACf1ef8fb2ded21784e7354aaf2e33ceae"
-auth_token = "09ab2d190d0cd98acc322254c3943438"
-client = Client(accont_sid,auth_token)
-client.messages.create(from_="+16064412876",body="merda",to="+5518981288077")
